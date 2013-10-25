f96195a8b2988837effb9a11a47636f865a5629a
Colin Gourlay <colin.j.gourlay@gmail.com>
2013-10-25 19:01:49 +1000

    Added nba-teams set. Not included in the menu yet.

--- src/js/color-sets/index.js
+++ src/js/color-sets/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     'car-brands': require('./car-brands'),
     'mlb-teams': require('./mlb-teams'),
+    'nba-teams': require('./nba-teams'),
     'nfl-teams': require('./nfl-teams'),
     'tech-brands': require('./tech-brands')
 };
---
+++ src/js/color-sets/nba-teams.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "Atlanta Hawks": "01244C", // blue
+    "Atlanta Hawks ": "D21033", // red
+    "Boston Celtics": "05854C", // green
+    "Charlotte Bobcats": "F26532", // orange
+    "Charlotte Bobcats ": "29588B", // blue
+    "Chicago Bulls": "D4001F", // red
+    "Cleveland Cavaliers": "9F1425", // red
+    "Cleveland Cavaliers ": "B99D6A", // yellow
+    "Dallas Mavericks": "006AB5", // blue
+    "Denver Nuggets": "4393D1", // blue
+    "Denver Nuggets ": "FBB529", // yellow
+    "Detroit Pistons": "ED174C", // red
+    "Detroit Pistons ": "006BB6", // blue
+    "Golden State Warriors": "002942", // blue
+    "Golden State Warriors ": "FFC33C", // yellow
+    "Houston Rockets": "CC0000", // red
+    "Indiana Pacers": "002E62", // blue
+    "Indiana Pacers ": "FFC225", // yellow
+    "Los Angeles Clippers": "EE2944", // red
+    "Los Angeles Clippers ": "146AA2", // blue
+    "Los Angeles Lakers": "4A2583", // purple
+    "Los Angeles Lakers ": "F5AF1B", // yellow
+    "Memphis Grizzlies": "001B41", // blue
+    "Miami Heat": "B62630", // red
+    "Miami Heat ": "FF9F00", // yellow
+    "Milwaukee Bucks": "00330A", // green
+    "Milwaukee Bucks ": "C82A39", // red
+    "Minnesota Timberwolves": "015287", // blue
+    "New Jersey Nets": "002258", // blue
+    "New Jersey Nets ": "D32E4C", // red
+    "New Orleans Pelicans": "0095CA", // blue
+    "New Orleans Pelicans ": "FEBB25", // yellow
+    "New York Knicks": "2E66B2", // blue
+    "New York Knicks ": "FF5C2B", // orange
+    "Oklahoma City Thunder": "0075C1", // blue
+    "Oklahoma City Thunder ": "E7442A", // orange
+    "Orlando Magic": "077ABD", // blue
+    "Philadelphia 76ers": "C5003D", // red
+    "Philadelphia 76ers ": "C7974D", // yellow
+    "Phoenix Suns": "48286C", // purple
+    "Phoenix Suns ": "FF7A31", // orange
+    "Portland Trail Blazers": "E1393E", // red
+    // "San Antonio Spurs": "BEC8C9", // grey
+    "Sacramento Kings": "743389", // purple
+    "Toronto Raptors": "CD1041", // red
+    "Utah Jazz": "001D4D", // blue
+    "Utah Jazz ": "480975", // purple
+    "Washington Wizards": "004874", // blue
+    "Washington Wizards ": "BC9B6A" // yellow
+};
