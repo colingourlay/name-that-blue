cbedc8d607a45165ad78f0fa9b26ce63a05bd86d
Colin Gourlay <colin@colin-gourlay.com>
2024-02-18 12:47:09 +1000

    Remove Twitter link

--- src/js/nav.js
+++ src/js/nav.js
@@ -18,9 +18,17 @@ module.exports = function (document) {
       { href: '.', class: !currentColorSet || currentColorSet === 'tech-brands' ? 'current' : '' },
       'Tech Brands'
     ),
-    crel('a', { href: '?nfl-teams', class: currentColorSet === 'nfl-teams' ? 'current' : '' }, 'NFL Teams'),
-    crel('a', { href: '?nhl-teams', class: currentColorSet === 'nhl-teams' ? 'current' : '' }, 'NHL Teams'),
-    crel('a', { href: 'http://twitter.com/collypops', target: '_blank', class: 'twitter' }, '@collypops'),
+    crel(
+      'a',
+      { href: '?nfl-teams', class: currentColorSet === 'nfl-teams' ? 'current' : '' },
+      'NFL Teams'
+    ),
+    crel(
+      'a',
+      { href: '?nhl-teams', class: currentColorSet === 'nhl-teams' ? 'current' : '' },
+      'NHL Teams'
+    ),
+    // crel('a', { href: 'http://twitter.com/collypops', target: '_blank', class: 'twitter' }, '@collypops'),
     // crel('a', {href: 'https://www.facebook.com/NameThatBlue', target: '_blank', 'class': 'facebook'}, 'FB.com/NameThatBlue'),
     crel(
       'a',
@@ -32,7 +40,8 @@ module.exports = function (document) {
   toggleEl = crel('a', { class: 'nav_toggle' }, crel('div', { class: 'nav_toggle__icon' }));
   toggleEl.onclick = function toggle() {
     navEl.className = 'nav' + (navEl.className.length > 3 ? '' : ' nav-open');
-    toggleEl.className = 'nav_toggle' + (toggleEl.className.length > 14 ? '' : ' nav_toggle-active');
+    toggleEl.className =
+      'nav_toggle' + (toggleEl.className.length > 14 ? '' : ' nav_toggle-active');
   };
 
   document.body.appendChild(navEl);
